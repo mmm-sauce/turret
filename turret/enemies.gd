cad77a4276cb8c3eea3eeeb79b85ed1bd1f177ad
NR1062 <716338@sd61learn.ca>
2024-10-28 13:04:11 -0700

    spawner

--- turret/enemies.gd
+++ turret/enemies.gd
@@ -1,6 +1,5 @@
-extends Node2D
+extends Area2D
 
-var Enemies = preload("res://enemies.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.y = position.y +10
+	
 
 
-$Spawner6func _on_timer_timeout() -> void:
-	var newenemies = Enemies.instaniate()
-	add_child(newenemies)
-	newenemies.position.x = randi_range(0,get_viewport().size.x)
-	
-	
+func _on_body_entered(body: Node2D) -> void:
+	get_parent().get_parent().get_node("label")
+	queue_free()
