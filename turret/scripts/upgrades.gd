0bd84a201f5b0d723d82b0adf0438cd8b948512c
Jovis <150299519+mmm-sauce@users.noreply.github.com>
2024-11-01 13:08:00 -0700

    UI tweeks

--- turret/scripts/upgrades.gd
+++ turret/scripts/upgrades.gd
@@ -7,10 +7,11 @@ var cdCost = 10
 var bsCost = 10
 
 var speed = .2
-var seperation = -100
+var Vseperation = -100
 
 var popup = false
 var popping = false
+var popArray = []
 
 
 func _process(delta: float) -> void:
@@ -20,12 +21,21 @@ func _process(delta: float) -> void:
 		get_parent().get_tree().paused = false
 	print(popup)
 	
-	$Upgrades.add_theme_constant_override("separation", seperation)
+	$Popup.rotation = Vseperation/-100 * 3.15
 	
-	$Upgrades/tsCost.text = str(tsCost)
-	$Upgrades/cdCost.text = str(cdCost)
-	$Upgrades/bsCost.text = str(bsCost)
+	$Sprite2D2.set_modulate(Color(1,1,1,(Vseperation+100)/100))
+	$Upgrades.add_theme_constant_override("v_separation", Vseperation)
+	
+	$Upgrades/tsCost.text = "turn speed - "+str(tsCost)+" coins"
+	$Upgrades/cdCost.text = "burst cooldown - "+str(cdCost)+" coins"
+	$Upgrades/bsCost.text = "burst shots - "+str(bsCost)+" coins"
 	$Coins.text = str(get_parent().coins)
+	
+	if get_parent().coins > min(bsCost,cdCost,tsCost) && popup == true:
+		for i in range(0, 101, 1):
+			await get_tree().create_timer(.005).timeout
+			$Popup.rotation = EasingFunctions.ease_in_out_bounce(3.05, 3.76, float(i)/100)
+			print($Popup.rotation)
 	
 
 func _on_turn_speed_pressed() -> void:
@@ -52,15 +62,14 @@ func _on_popup_pressed() -> void:
 	if not popping:
 		popping = true
 		# Changes step and max based on whether the menu is already out or not
-		
 		if popup == true: # seperation = 0
 			for i in range(0, 101, 1):
 				await get_tree().create_timer(.005).timeout
-				seperation = EasingFunctions.ease_in_out_cubic(0, -100, float(i)/100)
+				Vseperation = EasingFunctions.ease_in_out_cubic(0, -100, float(i)/100)
 			popup = false
 		else:
 			popup = true
 			for i in range(0, 101, 1):
 				await get_tree().create_timer(.005).timeout
-				seperation = EasingFunctions.ease_out_bounce(-100, 0, float(i)/100)
+				Vseperation = EasingFunctions.ease_out_bounce(-100, 0, float(i)/100)
 		popping = false
