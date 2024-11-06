96c04d65c3da92c8118403252f157bdeb0247c0f
Jovis <150299519+mmm-sauce@users.noreply.github.com>
2024-11-06 13:36:19 -0800

    Basic changes,
    prepping to publish half finished version to allow others to edit

--- turret/scripts/enimies.gd
+++ turret/scripts/enimies.gd
@@ -5,7 +5,6 @@ var toSpawn = 0 # 1 for plane, 2 for strong plane 3 for tank, 4 for strong tank
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	
 	$Plane.visible = false
 	$"Strong Plane".visible = false
 	$Tank.visible = false
