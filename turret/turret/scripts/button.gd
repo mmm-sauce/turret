d52a4e1409d92d84716142c1593a30fc0b7be51c
HG-K7771 <153408528+HG-K7771@users.noreply.github.com>
2024-11-14 15:03:43 -0800

    Completed game fix

--- turret/turret/scripts/button.gd
+++ turret/turret/scripts/button.gd
@@ -5,13 +5,13 @@ func _process(delta: float) -> void:
 	if get_parent().get_node("cannon").health < 0:
 		get_parent().get_child(0).get_child(6).play()
 		get_tree().paused = true
-		get_parent().get_node("Button").visible = true
+		self.visible = true
 		get_parent().get_node("UI").visible = false
 
 func _on_button_pressed() -> void:
 	get_tree().paused = false
 	get_parent().get_node("Spawner").reset_game()
-	get_parent().get_node("Button").visible = false
+	self.visible = false
 	get_parent().get_node("UI").visible = true
 	get_parent().coins = 0
 	get_parent().get_node("UI").reset()
